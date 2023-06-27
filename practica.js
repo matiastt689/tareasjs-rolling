d931fab34a0689a6ccbc62f7dc92115d88819bb9
Matias Tejedor <matiastt@gmail.com>
2023-06-27 23:47:44 +0200

    segundo commit

---
+++ practica.js
@@ -0,0 +1,43 @@
+let nombre = "matias"
+let edad = 33
+let segundo_nombre = "Tomas"
+segundo_nombre = "matiasT"
+console.log(segundo_nombre)
+
+const comision = "62i"
+console.log(comision)
+
+let Sin_definir = "null"
+
+console.log(nombre + " " + Sin_definir)
+
+console.log ("Mi edad es" +" " + edad +" "+ "años")
+
+console.log (nombre.at(1))
+
+console.log (nombre.at(0) + nombre.at(1) + nombre.at(2) + nombre.substring(3))
+
+console.log (segundo_nombre.toUpperCase(1))
+
+//Tarea
+// Crear dos variables de tipo number
+
+let num1 = 9;
+let num2 = 20;
+let num3 = 30
+let num4 = 10
+
+// mostrar por consola los resultados de las siguientes operaciones:
+// Sumar las dos variables
+console.log(num1 + num2 / num3);
+// Restar las dos variables
+console.log(num1 - num2 * num1 + num3 + num3*num2);
+// Multiplicar las dos variables
+console.log(num1 * num2 - num1*num3);
+// Dividir ambas variables
+console.log(num1 / num2 / num3 * (num2+num2));
+// comparar si la primera variable es mayor o igual que la segunda
+console.log(num1 >= num3);
+// Comparar si la primera variable es distinta de la segunda
+console.log(num1 != num2);
+
