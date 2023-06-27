90e7197dc334abb6d599d3dec82dd8aed9dab90f
Matias Tejedor <matiastt@gmail.com>
2023-06-27 22:52:34 +0200

    Primer Commit tareas

---
+++ tarea.js
@@ -0,0 +1,23 @@
+
+// Tarea 1
+// ------------
+// 1 - ingreso el apellido
+// 2 - calcular cantidad de caracteres
+// 3 - obtener la última letra del apellido
+
+let apellido = "tejedor"
+console.log("El apellido tiene " + apellido.length + " Caracteres");
+console.log(apellido.length)
+console.log(apellido.substring(6, 7));
+
+// Tarea 2
+// ------------
+// Dada la siguiente palabra "marmota"
+// 1 - Pasarla a mayuscula
+// 2 - Capitalizar la palabra (Marmota)
+
+let animal = "marmota"
+console.log(animal.toUpperCase())
+console.log(animal[0].toUpperCase(1) + animal.substring(1))
+
+
